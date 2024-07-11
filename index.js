221ce16549bceaa44fead47adb376207b45cd228
abhishek45768 <abhishekbhardwaj0046@gmail.com>
2024-07-11 14:01:34 +0530

    first commit

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const { PrismaClient } = require('@prisma/client');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 require('dotenv').config();
-
+const corsConfig={
+  origin:"*",
+  credential:true,
+  methods:["GET","POST","PUT","DELETE"],
+};
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsConfig));
 
 // Nodemailer Transporter Configuration
 const transporter = nodemailer.createTransport({
